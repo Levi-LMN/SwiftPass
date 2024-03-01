e48ad947aa0bb7e64ee653cd15a9e80ed827bef8
Levi Mukuha <117360388+Levi-LMN@users.noreply.github.com>
2024-03-01 19:14:22 +0300

    layout changes

--- index.php
+++ index.php
@@ -31,129 +31,7 @@ if ($loggedIn) {
 
 
 
-<!-- carousel -->
-<div class="carousel" style="margin-top: -20px; font-family: Poppins,serif; font-size: 12px; ">
-    <!-- list item -->
-    <div class="list">
-        <div class="item">
-            <img src="https://img.freepik.com/free-photo/traffic-vehicle-urban-reflections-city_1112-973.jpg?w=996&t=st=1709198573~exp=1709199173~hmac=411baf48b2329915de6005ec89148ef08c979f039c4908dfb84678c76c5d2d93">
-            <div class="content">
-                <div class="author">GiorgioGT</div>
-                <div class="title">SWIFTPASS</div>
-                <div class="topic">VARIOUS CLASS</div>
-                <div class="des">
-                    <!-- lorem 50 -->
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                </div>
-                <div class="buttons">
-                    <button>SEE MORE</button>
-                    <button>SUBSCRIBE</button>
-                </div>
-            </div>
-        </div>
-        <div class="item">
-            <img src="https://img.freepik.com/free-photo/interior-public-bus-transport_53876-63434.jpg?size=626&ext=jpg&ga=GA1.1.1345868282.1709198552&semt=ais">
-            <div class="content">
-                <div class="author">giorgioGT</div>
-                <div class="title">SWIFTPASS</div>
-                <div class="topic">Inside</div>
-                <div class="des">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                </div>
-                <div class="buttons">
-                    <button>SEE MORE</button>
-                    <button>SUBSCRIBE</button>
-                </div>
-            </div>
-        </div>
-        <div class="item">
-            <img src="https://img.freepik.com/premium-photo/bus-with-geometric-shapes-decoration_161488-522.jpg?size=626&ext=jpg&ga=GA1.1.1345868282.1709198552&semt=ais">
-            <div class="content">
-                <div class="author">giorgioGT</div>
-                <div class="title">SWIFTPASS</div>
-                <div class="topic">white bus</div>
-                <div class="des">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                </div>
-                <div class="buttons">
-                    <button>SEE MORE</button>
-                    <button>SUBSCRIBE</button>
-                </div>
-            </div>
-        </div>
-        <div class="item">
-            <img src="https://img.freepik.com/free-photo/close-up-hand-holding-bus-bar_23-2148921682.jpg?w=996&t=st=1709199620~exp=1709200220~hmac=8b43ea30de7150429067595d232cfa09d6139315c71fd5a89325e2246cef5a4b">
-            <div class="content">
-                <div class="author">giorgioGT</div>
-                <div class="title">SWIFTPASS</div>
-                <div class="topic">Hand holding bus bar</div>
-                <div class="des">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                </div>
-                <div class="buttons">
-                    <button>SEE MORE</button>
-                    <button>SUBSCRIBE</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- list thumnail -->
-    <div class="thumbnail">
-        <div class="item">
-            <img src="https://raw.githubusercontent.com/HoanghoDev/slider_1/main/image/img1.jpg">
-            <div class="content">
-                <div class="title">
-                    Name Slider
-                </div>
-                <div class="description">
-                    Description
-                </div>
-            </div>
-        </div>
-        <div class="item">
-            <img src="https://raw.githubusercontent.com/HoanghoDev/slider_1/main/image/img2.jpg">
-            <div class="content">
-                <div class="title">
-                    Name Slider
-                </div>
-                <div class="description">
-                    Description
-                </div>
-            </div>
-        </div>
-        <div class="item">
-            <img src="https://raw.githubusercontent.com/HoanghoDev/slider_1/main/image/img3.jpg">
-            <div class="content">
-                <div class="title">
-                    Name Slider
-                </div>
-                <div class="description">
-                    Description
-                </div>
-            </div>
-        </div>
-        <div class="item">
-            <img src="https://img.freepik.com/free-photo/close-up-hand-holding-bus-bar_23-2148921682.jpg?w=996&t=st=1709199620~exp=1709200220~hmac=8b43ea30de7150429067595d232cfa09d6139315c71fd5a89325e2246cef5a4b">
-            <div class="content">
-                <div class="title">
-                    Hand holding bus bar
-                </div>
-                <div class="description">
-                    Description
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- next prev -->
 
-    <div class="arrows">
-        <button id="prev"><</button>
-        <button id="next">></button>
-    </div>
-    <!-- time running -->
-    <div class="time"></div>
-</div>
-<!-- end carousel -->
 
 
 
@@ -169,8 +47,142 @@ if ($loggedIn) {
             <a href="auth/logout.php" class="btn btn-warning">Logout</a>
 
         </div>
+        <!--user bookings-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Your Bookings</h2>
+                    <p>View all your bookings.</p>
+                    <a href="user/user_bookings.php" class="btn btn-primary">View Bookings</a>
+                </div>
+            </div>
+        </div>
 
     <?php else: ?>
+
+        <!-- carousel -->
+        <div class="carousel" style="margin-top: -20px; font-family: Poppins,serif; font-size: 12px; ">
+            <!-- list item -->
+            <div class="list">
+                <div class="item">
+                    <img src="https://img.freepik.com/free-photo/traffic-vehicle-urban-reflections-city_1112-973.jpg?w=996&t=st=1709198573~exp=1709199173~hmac=411baf48b2329915de6005ec89148ef08c979f039c4908dfb84678c76c5d2d93">
+                    <div class="content">
+                        <div class="author">GiorgioGT</div>
+                        <div class="title">SWIFTPASS</div>
+                        <div class="topic">VARIOUS CLASS</div>
+                        <div class="des">
+                            <!-- lorem 50 -->
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+                        </div>
+                        <div class="buttons">
+                            <button>SEE MORE</button>
+                            <button>SUBSCRIBE</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="https://img.freepik.com/free-photo/interior-public-bus-transport_53876-63434.jpg?size=626&ext=jpg&ga=GA1.1.1345868282.1709198552&semt=ais">
+                    <div class="content">
+                        <div class="author">giorgioGT</div>
+                        <div class="title">SWIFTPASS</div>
+                        <div class="topic">Inside</div>
+                        <div class="des">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+                        </div>
+                        <div class="buttons">
+                            <button>SEE MORE</button>
+                            <button>SUBSCRIBE</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="https://img.freepik.com/premium-photo/bus-with-geometric-shapes-decoration_161488-522.jpg?size=626&ext=jpg&ga=GA1.1.1345868282.1709198552&semt=ais">
+                    <div class="content">
+                        <div class="author">giorgioGT</div>
+                        <div class="title">SWIFTPASS</div>
+                        <div class="topic">white bus</div>
+                        <div class="des">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+                        </div>
+                        <div class="buttons">
+                            <button>SEE MORE</button>
+                            <button>SUBSCRIBE</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="https://img.freepik.com/free-photo/close-up-hand-holding-bus-bar_23-2148921682.jpg?w=996&t=st=1709199620~exp=1709200220~hmac=8b43ea30de7150429067595d232cfa09d6139315c71fd5a89325e2246cef5a4b">
+                    <div class="content">
+                        <div class="author">giorgioGT</div>
+                        <div class="title">SWIFTPASS</div>
+                        <div class="topic">Hand holding bus bar</div>
+                        <div class="des">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+                        </div>
+                        <div class="buttons">
+                            <button>SEE MORE</button>
+                            <button>SUBSCRIBE</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- list thumnail -->
+            <div class="thumbnail">
+                <div class="item">
+                    <img src="https://raw.githubusercontent.com/HoanghoDev/slider_1/main/image/img1.jpg">
+                    <div class="content">
+                        <div class="title">
+                            Name Slider
+                        </div>
+                        <div class="description">
+                            Description
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="https://raw.githubusercontent.com/HoanghoDev/slider_1/main/image/img2.jpg">
+                    <div class="content">
+                        <div class="title">
+                            Name Slider
+                        </div>
+                        <div class="description">
+                            Description
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="https://raw.githubusercontent.com/HoanghoDev/slider_1/main/image/img3.jpg">
+                    <div class="content">
+                        <div class="title">
+                            Name Slider
+                        </div>
+                        <div class="description">
+                            Description
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="https://img.freepik.com/free-photo/close-up-hand-holding-bus-bar_23-2148921682.jpg?w=996&t=st=1709199620~exp=1709200220~hmac=8b43ea30de7150429067595d232cfa09d6139315c71fd5a89325e2246cef5a4b">
+                    <div class="content">
+                        <div class="title">
+                            Hand holding bus bar
+                        </div>
+                        <div class="description">
+                            Description
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- next prev -->
+
+            <div class="arrows">
+                <button id="prev"><</button>
+                <button id="next">></button>
+            </div>
+            <!-- time running -->
+            <div class="time"></div>
+        </div>
+        <!-- end carousel -->
 
 
 
@@ -198,16 +210,7 @@ if ($loggedIn) {
     </div>
 </div>
 
-<!--user bookings-->
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h2>Your Bookings</h2>
-            <p>View all your bookings.</p>
-            <a href="user/user_bookings.php" class="btn btn-primary">View Bookings</a>
-        </div>
-    </div>
-</div>
+
 
 
 <?php
--- user/user_bookings.php
+++ user/user_bookings.php
@@ -1,4 +1,9 @@
 <?php
+// Set page title for the layout
+$pageTitle = "User bookings Page";
+
+// Content for the layout
+ob_start();
 // Include your database connection code here
 include '../auth/database.php'; // Update with your actual database connection file
 
@@ -30,38 +35,84 @@ if (!$bookingsResult) {
     header("Location: error.php?message=$errorMessage");
     exit();
 }
-
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Bookings</title>
-    <!-- Add any additional styles or meta tags here -->
-</head>
-<body>
-<h1>Your Bookings</h1>
 
-<?php
-if (mysqli_num_rows($bookingsResult) > 0) {
-    // Display the user's bookings
-    while ($booking = mysqli_fetch_assoc($bookingsResult)) {
-        echo "<p>";
-        echo "Ticket Number: {$booking['ticket_number']}<br>";
-        echo "Departure Time: {$booking['departure_time']}<br>";
-        echo "Vehicle: {$booking['make']} {$booking['model']}<br>";
-        echo "Sacco: {$booking['sacco_name']}<br>";
-        echo "Seat Number: {$booking['seat_number']}<br>";
-        echo "Price: {$booking['price']}<br>";
-        echo "</p>";
+<div class="container mt-5">
+    <h1 class="mb-4">Your Bookings</h1>
+
+    <?php
+    if (mysqli_num_rows($bookingsResult) > 0) {
+        // Group bookings by schedule
+        $groupedBookings = [];
+        while ($booking = mysqli_fetch_assoc($bookingsResult)) {
+            $scheduleId = $booking['travel_schedule_id'];
+
+            // Fetch departure location and destination from travelschedule table
+            $scheduleQuery = "SELECT departure_location, destination, departure_time FROM travelschedule WHERE id = $scheduleId";
+            $scheduleResult = mysqli_query($conn, $scheduleQuery);
+            $scheduleData = mysqli_fetch_assoc($scheduleResult);
+
+            $booking['departure_location'] = $scheduleData['departure_location'] ?? 'N/A';
+            $booking['destination'] = $scheduleData['destination'] ?? 'N/A';
+
+            $groupedBookings[$booking['departure_time']][] = $booking;
+        }
+
+        // Display each group
+        foreach ($groupedBookings as $schedule => $bookings) {
+            $formattedSchedule = date("l jS F Y g:i A", strtotime($schedule));
+
+            echo "<div class='mb-4'>";
+            echo "<h2 class='mb-3'>Schedule: $formattedSchedule</h2>";
+            echo "<p class='mb-3'>From: {$bookings[0]['departure_location']} | To: {$bookings[0]['destination']}</p>";
+
+            $counter = 0; // Counter for tracking the number of bookings in the current row
+
+            echo "<div class='row'>";
+
+            foreach ($bookings as $booking) {
+                echo "<div class='col-md-4 mb-4'>";
+                echo "<div class='card h-100 shadow border-primary'>";
+                echo "<div class='card-body'>";
+                echo "<h5 class='card-title font-weight-bold mb-3'>Ticket Number: {$booking['ticket_number']}</h5>";
+                echo "<ul class='list-unstyled'>";
+                echo "<li>Departure Time: {$booking['departure_time']}</li>";
+                echo "<li>Vehicle: {$booking['make']} {$booking['model']}</li>";
+                echo "<li>Sacco: {$booking['sacco_name']}</li>";
+                echo "<li>Seat Number: {$booking['seat_number']}</li>";
+                echo "<li>Price: {$booking['price']}</li>";
+                echo "</ul>";
+                echo "</div>";
+                echo "</div>";
+                echo "</div>";
+
+                // End the row for every third booking
+                if ($counter % 3 == 2 || $counter == count($bookings) - 1) {
+                    echo "</div>"; // Close the row
+                    echo "<div class='row'>"; // Start a new row
+                }
+
+                $counter++;
+            }
+
+            echo "</div>"; // Close the div containing the schedule
+            echo "</div>"; // Close the div for each schedule
+        }
+    } else {
+        echo "<p>No bookings found.</p>";
     }
-} else {
-    echo "<p>No bookings found.</p>";
-}
-?>
+    ?>
+</div>
+
+
+
+
 
 <!-- Add any additional content or links here -->
+<?php
+// Get the buffered content and assign it to $content
+$pageContent = ob_get_clean();
 
-</body>
-</html>
+// Include the layout
+include('../layout.php');
+?>
